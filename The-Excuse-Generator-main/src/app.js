90088475f43baaeda8aa79d839c7d2425032b6c4
santiago ceron <77759631+santiagoceron10@users.noreply.github.com>
2021-02-24 19:38:48 -0500

    Add files via upload

---
+++ The-Excuse-Generator-main/src/app.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  console.log("who" + "action" + "what" + "when");
+};
