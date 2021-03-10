2595cb3e52d8784223a2d736a6ae088a3f0b23c1
santiago ceron <77759631+santiagoceron10@users.noreply.github.com>
2021-03-10 10:47:41 -0500

    Commit1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
@@ -16,5 +16,12 @@ window.onload = function() {
     "during my lunch",
     "while I was praying"
   ];
-  console.log("who" + "action" + "what" + "when");
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  let excuse = document.querySelector("h3");
+
+  excuse.innerHTML =
+    who[whoIndex] + " " + action[actionIndex] + " " + " " + when[whenIndex];
 };
